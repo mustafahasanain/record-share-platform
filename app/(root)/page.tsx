68d3d7439b21d2e4cd0ea9fb3e3971b3397d0a6a
Mustafa Hasanain <m7asanain@gmail.com>
2025-05-12 13:55:21 +0300

    retrieve dummy cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,17 @@
 import Header from "@/components/Header";
 import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 
 const Page = () => {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library" />
 
-      <VideoCard
-        id="1"
-        title="SnapChat Messages"
-        thumbnail="/assets/samples/thumbnail (1).png"
-        createdAt={new Date("2025-05-01")}
-        userImg="/assets/images/jason.png"
-        username="Jason"
-        views={10}
-        visibility="public"
-        duration={156}
-      />
+      <section className="video-grid">
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   );
 };
