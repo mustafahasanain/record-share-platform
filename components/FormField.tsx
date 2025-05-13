2d5439fe2bbe3dd683df09a70db6a89231d9a4fa
Mustafa Hasanain <m7asanain@gmail.com>
2025-05-13 21:35:20 +0300

    fix: prevent input from losing focus due to continuous re-rendering

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -8,9 +8,11 @@ const FormField = ({
   as = "input",
   options = [],
 }: FormFieldProps) => {
-  const InputToRender = ({ type }: { type: string }) => {
-    if (type === "textarea") {
-      return (
+  return (
+    <div className="form-field">
+      <label htmlFor={id}>{label}</label>
+
+      {as === "textarea" ? (
         <textarea
           id={id}
           name={id}
@@ -18,9 +20,7 @@ const FormField = ({
           onChange={onChange}
           placeholder={placeholder}
         />
-      );
-    } else if (type === "select") {
-      return (
+      ) : as === "select" ? (
         <select id={id} name={id} value={value} onChange={onChange}>
           {options.map(({ label, value }) => (
             <option key={label} value={value}>
@@ -28,9 +28,7 @@ const FormField = ({
             </option>
           ))}
         </select>
-      );
-    } else {
-      return (
+      ) : (
         <input
           id={id}
           name={id}
@@ -38,15 +36,7 @@ const FormField = ({
           onChange={onChange}
           placeholder={placeholder}
         />
-      );
-    }
-  };
-
-  return (
-    <div className="form-field">
-      <label htmlFor={id}>{label}</label>
-
-      <InputToRender type={as} />
+      )}
     </div>
   );
 };
